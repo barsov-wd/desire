00ba8eeb0feaa23f06655e5fb2db80b498a2c24b
Victor <barosv@yandex.ru>
2022-07-07 16:02:48 +0400

    Сверстаны несколько секций без адаптива

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+    });
+});
 
-})
+var mixer = mixitup('.gallery__inner', {
+    load: {
+        filter: '.living'
+    }
+});
